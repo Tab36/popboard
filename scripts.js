625d5898443afcc09273858d91e31c3798797dfe
Tab36 <168956404+Tab36@users.noreply.github.com>
2026-02-08 17:22:47 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const API = "https://popboard.seojin8550.workers.dev";
-async function login() {
+async function gettoken() {
   const res = await fetch(API + "/auth/issue");
   const _token = await res.text();
   localStorage.setItem("token", _token);
 }
+gettoken()
 const token = localStorage.getItem("token")
 
 async function api(path, options = {}) {
