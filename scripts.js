27449b9f2866674f83ce3769f1acf101b431aaf4
Tab36 <168956404+Tab36@users.noreply.github.com>
2026-02-16 16:54:57 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 const API = "https://popboard.seojin8550.workers.dev";
-async function gettoken() {
-  const res = await fetch(API + "/auth/issue");
-    const _token = await res.text();
-  localStorage.setItem("token", _token);
-}
-gettoken()
-const token = localStorage.getItem("token")
 
 async function api(path, options = {}) {
+  const token = localStorage.getItem("token");
+
   const res = await fetch(API + path, {
     ...options,
     headers: {
@@ -24,24 +19,23 @@ document.getElementById("login-form").addEventListener("submit", async (e) => {
 
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-
-  async function login(username, password) {
-  const res = await fetch(API + "/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password })
-  });
-
-  return res;
-}
-
   const message = document.getElementById("message");
 
-  if (res.ok) {
-    const token = await res.text();
-    localStorage.setItem("token", token);
-    message.textContent = "Login successful.";
-  } else {
-    message.textContent = "Login failed.";
+  try {
+    const res = await fetch(API + "/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    });
+
+    if (res.ok) {
+      const token = await res.text();
+      localStorage.setItem("token", token);
+      message.textContent = "Login successful.";
+    } else {
+      message.textContent = "Login failed.";
+    }
+  } catch (err) {
+    message.textContent = "Server error.";
   }
 });
